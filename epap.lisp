fe00a2c6a66a17e57e214edf6c202191ec821f7b
Mikael Brockman <mikael@brockman.se>
2021-11-30 16:32:20 +0200

    <mbrock> 2 files changed (+22/-10)

--- epap.lisp
+++ epap.lisp
@@ -1,12 +1,24 @@
 (require :asdf)
 (asdf:load-system :cffi)
 
-(defpackage :epap
-  (:use :common-lisp :cffi))
+(defpackage :epapi
+  (:use :common-lisp :cffi :asdf :uiop))
 
-(in-package :epap)
+(in-package :epapi)
 
 (define-foreign-library epapi
   (:unix "./zig-out/lib/libepapi.so"))
 
 (use-foreign-library epapi)
+
+(defun reload ()
+  (uiop:run-program "make")
+  (use-foreign-library epapi)
+  )
+
+
+(defcfun "epap_start_broadcom" :uint32)
+(defcfun "epap_stop_broadcom" :uint32)
+
+(epap-start-broadcom)
+(epap-stop-broadcom)
--- epapi.zig
+++ epapi.zig
@@ -1,11 +1,11 @@
 const epd = @import("./epd.zig");
 
-export fn start_broadcom() u32 {
-  epd.initializeBroadcomChip() catch |err| return 1;
-  return 0;
+export fn epap_start_broadcom() u32 {
+    epd.initializeBroadcomChip() catch |err| return 1;
+    return 0;
 }
 
-export fn stop_broadcom() u32 {
-  epd.finalizeBroadcomChip() catch |err| return 1;
-  return 0;
+export fn epap_stop_broadcom() u32 {
+    epd.finalizeBroadcomChip() catch |err| return 1;
+    return 0;
 }

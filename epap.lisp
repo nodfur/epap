cef06dfc4f56120376f159dcb60f2d373e9bbff9
Mikael Brockman <mikael@brockman.se>
2021-11-30 16:00:00 +0000

    <root> 3 files changed (+54/-4)

--- epap.lisp
+++ epap.lisp
@@ -12,13 +12,46 @@
 (use-foreign-library epapi)
 
 (defun reload ()
-  (uiop:run-program "make")
+  (uiop:run-program "make" :output t)
   (use-foreign-library epapi)
   )
 
-
 (defcfun "epap_start_broadcom" :uint32)
 (defcfun "epap_stop_broadcom" :uint32)
+(defcfun "epap_start_text" :uint32)
 
-(epap-start-broadcom)
-(epap-stop-broadcom)
+(defcstruct display-info
+  (panel-width :uint16)
+  (panel-height :uint16)
+  (memory-address :uint32)
+  (firmware-version :char :count 16)
+  (lut-version :char :count 16))
+
+(defcfun "epap_start_display" :uint32
+  (vcom :double)
+  (info (:pointer (:struct display-info))))
+
+(defcfun "epap_sleep" :uint32)
+
+(uiop:run-program "whoami" :output :string)
+
+(defvar *panel-width*)
+(defvar *panel-height*)
+
+(defun get-display-info ()
+  (with-foreign-object (info '(:struct display-info))
+    (epap-start-display -1.73d0 info)
+    (with-foreign-slots
+        ((panel-width panel-height)
+         info (:struct display-info))
+      (setf *panel-width* panel-width
+            *panel-height* panel-height)
+      (list panel-width panel-height))))
+
+(defun test ()
+  (epap-start-broadcom)
+  (get-display-info)
+  (epap-stop-broadcom)
+  )
+
+(test)
--- epapi.zig
+++ epapi.zig
@@ -1,4 +1,5 @@
 const epd = @import("./epd.zig");
+const text = @import("./freetype.zig");
 
 export fn epap_start_broadcom() u32 {
     epd.initializeBroadcomChip() catch |err| return 1;
@@ -7,5 +8,20 @@ export fn epap_start_broadcom() u32 {
 
 export fn epap_stop_broadcom() u32 {
     epd.finalizeBroadcomChip() catch |err| return 1;
+    return 0;
+}
+
+export fn epap_start_text() u32 {
+    text.init() catch |err| return 1;
+    return 0;
+}
+
+export fn epap_start_display(vcom: f64, info: *epd.SystemInfo) u32 {
+    info.* = epd.initializeDisplay(vcom) catch |err| return 1;
+    return 0;
+}
+
+export fn epap_sleep() u32 {
+    epd.sleep() catch |err| return 1;
     return 0;
 }
